6c92c3497bde99f26ad9cd4bb68dbc783632f067
Rafal Szubert <rafa.szubert@gamil.com>
2023-12-01 23:44:11 +0100

    adding sorting featue to POST request

--- src/main/java/pl/put/poznan/transformer/logic/SortingMadness.java
+++ src/main/java/pl/put/poznan/transformer/logic/SortingMadness.java
@@ -1,16 +1,22 @@
 package pl.put.poznan.transformer.logic;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
 public class SortingMadness {
 
-    private final String[] transforms;
+    private final String order;
 
-    public SortingMadness(String[] transforms){
-        this.transforms = transforms;
+    public SortingMadness(String order){
+        this.order = order;
     }
-
+    public List<Integer> quickSort(List<Integer> dataList){
+        Collections.sort(dataList);
+        return dataList;
+    }
     public String transform(String text){
         // of course, normally it would do something based on the transforms
         return text.toUpperCase();
--- src/main/java/pl/put/poznan/transformer/rest/SortingMadnessController.java
+++ src/main/java/pl/put/poznan/transformer/rest/SortingMadnessController.java
@@ -5,38 +5,38 @@ import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.SortingMadness;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/{text}")
+@RequestMapping("/")
 public class SortingMadnessController {
 
     private static final Logger logger = LoggerFactory.getLogger(SortingMadnessController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        SortingMadness transformer = new SortingMadness(transforms);
-        return transformer.transform(text);
-    }
+//    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+//    public String get(@PathVariable String text,
+//                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+//
+//        // log the parameters
+//        logger.debug(text);
+//        logger.debug(Arrays.toString(transforms));
+//
+//        // perform the transformation, you should run your logic here, below is just a silly example
+//        SortingMadness transformer = new SortingMadness("transofrms");
+//        return transformer.transform(text);
+//    }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
+    public List<Integer> post(@RequestParam(defaultValue = "ascending") String order, @RequestBody List<Integer> dataList) {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        logger.debug(dataList.toString());
+        logger.debug(order);
 
-        // perform the transformation, you should run your logic here, below is just a silly example
-        SortingMadness transformer = new SortingMadness(transforms);
-        return transformer.transform(text);
+        // Perform any other processing as needed
+        SortingMadness sortingMadness = new SortingMadness(order);
+        return sortingMadness.quickSort(dataList);
     }
 
 
