b81603a68f3b134953c1971b60201bb41024013f
Szymon Skowroński <szymon.skowronski.uczen17@zsl.poznan.pl>
2023-12-21 01:48:28 +0100

    feat: add controller tests

---
+++ src/test/java/pl/put/poznan/sorting_madness/rest/SortingMadnessServiceTest.java
@@ -0,0 +1,22 @@
+package pl.put.poznan.sorting_madness.rest;
+
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AllArgsConstructor
+class SortingMadnessServiceTest {
+
+    @Autowired
+    private SortingMadnessService sortingMadnessService;
+
+    @Test
+    void sortValues() {
+    }
+
+    @Test
+    void sortObjects() {
+    }
+}
